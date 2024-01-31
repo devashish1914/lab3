028eb9b9cf65f3bf974e9d60264f376564e259b5
devashish1914 <devashish.m@yahoo.com>
2024-01-31 13:02:25 -0800

    Corrected the errors in the ArrayTests.java file and added some tests to ArrayTests.java file

--- ArrayTests.java
+++ ArrayTests.java
@@ -9,10 +9,42 @@ public class ArrayTests {
     assertArrayEquals(new int[]{ 3 }, input1);
 	}
 
+  @Test 
+	public void testReverseInPlace2() {
+    int[] input1 = { 7,6,3 };
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{ 3,6,7 }, input1);
+	}
+
+  @Test 
+	public void testReverseInPlace3() {
+    int[] input1 = { 8,7,6,3 };
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{ 3,6,7,8 }, input1);
+	}
+
+  @Test 
+	public void testReverseInPlace4() {
+    int[] input1 = { };
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{ }, input1);
+	}
 
   @Test
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testReversed2() {
+    int[] input1 = {1,2,3,4,5 };
+    assertArrayEquals(new int[]{5,4,3,2,1 }, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testReversed3() {
+    int[] input1 = {1,2,3,4,5,6 };
+    assertArrayEquals(new int[]{6,5,4,3,2,1 }, ArrayExamples.reversed(input1));
   }
 }
